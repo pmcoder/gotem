3728aaffc1080521d78b7ad48b40603232ea2fca
Phillip Hagger <phagger@pmcoderlabs.com>
2014-10-20 11:10:37 -0500

    the beginning... gotem

---
+++ main.go
@@ -0,0 +1,299 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/dchest/uniuri"
+)
+
+//TO create a new file from deps already in place
+//1 Get deps from go -list -e -json
+//2 get non locals
+//3 walk non locals to find repo type and copy
+//3 check gotem dir for non locals
+//4 pull into gotem
+//5 link via existing gopath so that sym link schemes work
+//NOTE linking might only work *nix
+
+//TODO might be better to find dirs with repo info and find remotes
+var remotes []string = []string{"github"}
+
+type GoList struct {
+	Dir     string
+	Imports []string
+	Deps    []string
+}
+
+type RemoteDep struct {
+	ImportPath string
+	RepoType   string
+}
+
+type DepInfo struct {
+	DcvsType string
+	Version  string
+	Path     string
+}
+
+func getGoList() *GoList {
+	cmd := exec.Command("go", "list", "-e", "-json")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return nil
+	}
+
+	gList, err := buildGoList(out.Bytes())
+	if err != nil {
+
+	}
+
+	return gList
+
+}
+
+func findRemoteDeps(deps []string) []string {
+	remoteDeps := []string{}
+	for _, dep := range deps {
+		fmt.Println("Checking %s for remote", dep)
+		for _, remoteSign := range remotes {
+			if strings.Contains(dep, remoteSign) {
+				remoteDeps = append(remoteDeps, dep)
+				continue
+			}
+		}
+	}
+
+	return remoteDeps
+}
+
+func buildGoList(j []byte) (*GoList, error) {
+
+	gList := &GoList{}
+
+	err := json.Unmarshal(j, gList)
+	if err != nil {
+		return nil, err
+	}
+
+	return gList, nil
+}
+
+func getGoPath() string {
+
+	for _, env := range os.Environ() {
+		if strings.Contains(env, "GOPATH") {
+			g := strings.Split(env, "=")
+			if len(g) > 1 {
+				return g[1]
+			}
+		}
+	}
+
+	return ""
+
+}
+
+//return dep info
+func walkGoPathForRemotes(r []string, goPath string) {
+
+	depInfos := []DepInfo{}
+
+	goSrc := filepath.Join(goPath, "src")
+
+	for _, remote := range r {
+		remotePath := filepath.Join(goSrc, remote)
+		fmt.Printf("Checking path %s\n", remotePath)
+		//make sure the dir is there
+		_, err := os.Stat(remotePath)
+		if err != nil {
+			fmt.Println(err)
+			if os.IsNotExist(err) {
+				//mark as not go gotten... maybe even go get it
+				fmt.Printf("%s does not exist\n", remotePath)
+				continue
+			}
+		}
+		depInfo := processDep(remote, remotePath)
+		depInfos = append(depInfos, *depInfo)
+		//copy the dir
+		copyDep(remote, remotePath)
+		fmt.Println(remotePath)
+	}
+
+	saveDeps(depInfos)
+}
+
+func pathExists(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+func copyDep(remotePath, localPath string) error {
+
+	localPathInfo, err := os.Stat(localPath)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(filepath.Join("_gotem", remotePath), localPathInfo.Mode())
+	if err != nil {
+		fmt.Println("Could not make dirs under _gotem")
+		return err
+	}
+
+	srcDir, _ := os.Open(localPath)
+	object, err := srcDir.Readdir(-1)
+
+	for _, obj := range object {
+		srcFile := filepath.Join(localPath, obj.Name())
+		destFile := filepath.Join("_gotem", "remotePath", obj.Name())
+
+		if obj.IsDir() {
+			err = copyDep(remotePath, srcFile)
+			if err != nil {
+				return err
+			}
+		} else {
+			sf, err := os.Open(srcFile)
+			if err != nil {
+				return err
+			}
+			defer sf.Close()
+			df, err := os.Create(destFile)
+			if err != nil {
+				return err
+			}
+			defer df.Close()
+
+			_, err = io.Copy(df, sf)
+			if err != nil {
+				return err
+			}
+			srcInfo, err := os.Stat(srcFile)
+			if err != nil {
+				err = os.Chmod(destFile, srcInfo.Mode())
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func saveDeps(deps []DepInfo) error {
+	//check for existence of the gotem file
+	/*if !pathExists("gotem.json"){
+		//create the json file
+	}*/
+
+	gotem, err := os.Create("gotem.json")
+	if err != nil {
+		fmt.Println("Can't open or create gotem.json", err)
+	}
+
+	//write the dep info to file
+	b, err := json.Marshal(deps)
+	if err != nil {
+		fmt.Println("Error marshaling gotem in")
+		return err
+	}
+	_, err = gotem.Write(b)
+	if err != nil {
+		fmt.Println("Error writing gotem file")
+		return err
+	}
+
+	return nil
+
+}
+
+func getDepRepo(path string) string {
+
+	if pathExists(filepath.Join(path, ".git")) {
+		return "git"
+	} else if pathExists(filepath.Join(path, ".svn")) {
+		return "svn"
+	} else {
+		return ""
+	}
+
+}
+
+func addDepsToPath() {
+
+}
+
+func linkDepstToPath() {
+
+}
+
+func findDependencyVersion(depInfo *DepInfo) error {
+	//only handle git for now
+	if depInfo.DcvsType != "git" {
+		return errors.New("Unsupported repo type " + depInfo.DcvsType)
+	}
+
+	tagOut, err := exec.Command("git", "describe --tags").Output()
+	if err != nil {
+		fmt.Println("Error looking for tag... moving to revision num", err)
+		err = nil
+	} else {
+		if len(tagOut) > 0 {
+			depInfo.Version = string(tagOut)
+			return nil
+		}
+	}
+
+	idOut, err := exec.Command("git", "rev-parse HEAD").Output()
+	if err != nil {
+		fmt.Println("Error finding revision num.... giving up", err)
+		return err
+	}
+
+	if len(idOut) > 0 {
+		depInfo.Version = string(idOut)
+	} else {
+		fmt.Println("Could not get tag or version")
+		return errors.New("Could not get tag or version")
+	}
+	return nil
+}
+
+func processDep(remotePath, localPath string) *DepInfo {
+	//get dep repo and info
+	depInfo := DepInfo{DcvsType: getDepRepo(localPath), Path: remotePath}
+
+	findDependencyVersion(&depInfo)
+	//write dep to gotem file
+
+	//copy deps to gotem dir
+
+	//link the deps into the go path from the gotem dir or add a src to
+	//the gotem dir and move the deps under than and add the gotem dir to the patc
+
+	return &depInfo
+}
+
+func main() {
+	uniuri.New()
+	gl := getGoList()
+	fmt.Println("Deps:", gl.Deps)
+	r := findRemoteDeps(gl.Deps)
+	fmt.Printf("Remotes %v\n", r)
+	goPath := getGoPath()
+	fmt.Println(getGoPath())
+	walkGoPathForRemotes(r, goPath)
+}
